4cbde600f4e59d78e641b2aa9dfee168eef6ef80
Simon Kennedy <simon.kennedy@cmacgroup.co.uk>
2018-06-04 08:20:07 +0100

    Removed Identity UI as going to craft my own

--- src/Hamwic.Cif.Web/Startup.cs
+++ src/Hamwic.Cif.Web/Startup.cs
@@ -72,6 +72,10 @@ namespace Hamwic.Cif.Web
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
